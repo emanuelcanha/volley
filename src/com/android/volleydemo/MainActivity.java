7d98ea0a13b38445768af1e8e516eb491f221e7b
Emanuel <emanuelcanha@gmail.com>
2014-01-19 22:05:44 +0000

    added json and image request

--- src/com/android/volleydemo/MainActivity.java
+++ src/com/android/volleydemo/MainActivity.java
@@ -6,10 +6,11 @@ import org.json.JSONObject;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
-import com.android.players.R;
 import com.android.volley.Request.Method;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -18,86 +19,94 @@ import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.NetworkImageView;
 
-public class MainActivity extends Activity {
-	private static final String IMAGE_URL = "http://emanuelrc.herokuapp.com/assets/homer.png";
-	private static final String DATA_URL = "http://emanuelrc.herokuapp.com/players/1.json";
+public class MainActivity extends Activity implements OnClickListener {
+	private static final String DATA_URL = "http://graph.facebook.com/MadLabUK";
+	private static final String IMAGE_URL1 = "http://scontent-a.xx.fbcdn.net/hphotos-ash2/t1/s720x720/293792_541411519206596_1968206097_n.jpg";
+	private static final String IMAGE_URL2 = "https://fbcdn-sphotos-g-a.akamaihd.net/hphotos-ak-frc3/1461709_810088515672227_1726228651_n.jpg";
 
-	private MyApplication mMyApplication;
 	private RequestQueue mRequestQueue;
+	ImageLoader mImageLoader;
 
-	private TextView mName;
+	private TextView mName, mAbout;
 	private NetworkImageView mAvatar;
+	private Button mImageOne, mImageTwo;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		// MyApplication singleton object
-		mMyApplication = (MyApplication) getApplicationContext();
-
-		// RequestQueue singleton object
-		mRequestQueue = mMyApplication.getRequestQueue();
+		MyApplication myApplication = (MyApplication) getApplicationContext();
+		// get RequestQueue and ImageLoader
+		mRequestQueue = myApplication.getRequestQueue();
+		mImageLoader = myApplication.getImageLoader();
 
 		mName = (TextView) findViewById(R.id.textView_name);
 		mAvatar = (NetworkImageView) findViewById(R.id.networkImageView_avatar);
+		mAbout = (TextView) findViewById(R.id.textView_about);
+		mImageOne = (Button) findViewById(R.id.button_image1);
+		mImageOne.setOnClickListener(this);
+		mImageTwo = (Button) findViewById(R.id.button_image2);
+		mImageTwo.setOnClickListener(this);
 
 		getData();
-		getImage();
-
+		getImage(IMAGE_URL1);
 	}
 
-	// JsonObjectRequest jsonRequest = new JsonObjectRequest(method, url, jsonRequest, listener, errorListener)
 	private void getData() {
 
+		// JsonObjectRequest jsonRequest = new JsonObjectRequest(method, url,
+		// jsonObject, listener, errorListener)
 		JsonObjectRequest jsonRequest = new JsonObjectRequest(Method.GET, DATA_URL, null,
 				new Response.Listener<JSONObject>() {
 
 					@Override
 					public void onResponse(JSONObject response) {
-						setUserData(response);
+						setData(response);
 					}
 
 				}, new Response.ErrorListener() {
+
 					@Override
 					public void onErrorResponse(VolleyError error) {
-						Log.e("HTTP error: " + String.valueOf(error.networkResponse.statusCode),
-								"Something went wrong: " + error.getMessage());
-						// TODO: Handle errors
+						Log.e("Volley ERROR", "Message: " + error.getMessage());
 					}
 				});
 
 		mRequestQueue.add(jsonRequest);
 	}
 
-	private void getImage() {
-		ImageLoader imageLoader = mMyApplication.getImageLoader();
-		mAvatar.setImageUrl(IMAGE_URL, imageLoader);
-	}
-
-	private void setUserData(JSONObject data) {
+	private void setData(JSONObject data) {
 		String name = "";
+		String about = "";
 
 		try {
 			name = data.getString("name");
+			about = data.getString("about");
 
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
 
 		mName.setText(name);
+		mAbout.setText(about);
+	}
+
+	private void getImage(String imageUrl) {
+		// NetworkImageView.setImageUrl(url, imageLoader)
+		mAvatar.setImageUrl(imageUrl, mImageLoader);
 	}
 
 	@Override
-	protected void onStop() {
-		super.onStop();
-		mRequestQueue.cancelAll(mMyApplication);
-	}
+	public void onClick(View v) {
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		mName.setText("");
+		mAbout.setText("");
+		getData();
+		if (v == mImageOne) {
+			getImage(IMAGE_URL1);
+		} else {
+			getImage(IMAGE_URL2);
+		}
 	}
-
 }
